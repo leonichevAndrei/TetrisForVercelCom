c10170624da1f1531e9c3ec4ffc497238943c662
Andrei <leonichev.design@gmail.com>
2024-06-28 04:01:31 +0300

    with node_modules and dist

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 // Middleware for requests logging:
